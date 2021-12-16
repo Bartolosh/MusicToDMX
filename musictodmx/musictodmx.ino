cb93960114ada8a363775bda34bb674d2137b487
Sbaccio <federicosilvestri1998@virgilio.it>
2021-12-16 18:10:52 +0100

    add mic register task

--- musictodmx/musictodmx.ino
+++ musictodmx/musictodmx.ino
@@ -1,13 +1,30 @@
 #include <STM32FreeRTOS.h>
 #include <timers.h>
 #include <semphr.h>
+#include <stdlib.h>
 
+
+#define SAMPLES 2048
 
 SemaphoreHandle_t mtxToBuffer;
 
+float *buffer;
+
+// TODO check if the sample frequency is correct
 void taskInputRecording(void *pvParameters){
     while(true){
-
+        xSemaphoreTake(mtxToBuffer, portMAX_DELAY);
+        uint16_t c = 0;
+        while(c < SAMPLES){
+            buffer[c] = (float)analogRead(A0);
+            Serial.print("READ  ");
+            Serial.print(c);
+            Serial.print(": ");
+            Serial.println(buffer[c]);
+            c++;
+        }
+        Serial.println("BUFFER FULL");
+        xSemaphoreGive(mtxToBuffer);
         
     }
 }
@@ -15,7 +32,11 @@ void taskInputRecording(void *pvParameters){
 
 void setup(){
     Serial.begin(115200);
+    buffer = (float*)calloc(SAMPLES,sizeof(float));
+    mtxToBuffer = xSemaphoreCreateBinary();                               /* semaphores for buffer*/
+    xSemaphoreGive(mtxToBuffer);
 
+    xTaskCreate(taskInputRecording, "inputRec", 115, NULL, 0, NULL); 
 
     vTaskStartScheduler();                                                /* explicit call needed */
     Serial.println("Insufficient RAM");
@@ -23,10 +44,11 @@ void setup(){
 
 void loop(){
     //ERROR IF WE ARE HERE
+    /*
     TimerHandle_t xTimer = xTimerCreate("PPMGenerator", pdMS_TO_TICKS(FRAME_LENGTH), pdTRUE, 0, taskPPMGenerator);
     xTimerStart(xTimer, 0);  
 
-    semReadFlex = xSemaphoreCreateBinary();                               /* synchronization semaphores */
+    semReadFlex = xSemaphoreCreateBinary();                              
     xSemaphoreGive(semReadFlex);
 
     semDataProcess = xSemaphoreCreateCounting(2, 0);
@@ -37,5 +59,5 @@ void loop(){
 
     xSemaphoreTake(semReadFlex, portMAX_DELAY);
 
-    xSemaphoreGive(semDataProcess);
+    xSemaphoreGive(semDataProcess);*/
 }
