a0f48247dab4e8b4920e03c291fa7c34b38448c4
Sbaccio <federicosilvestri1998@virgilio.it>
2021-12-19 10:58:46 +0100

    add duoble declaration

--- musictodmx/manage_output.h
+++ musictodmx/manage_output.h
@@ -7,6 +7,14 @@
 #include <ArduinoRS485.h> // the ArduinoDMX library depends on ArduinoRS485
 #include <ArduinoDMX.h>
 
+extern eurolite_par par1;
+extern eurolite_par par2;
+extern eurolite_par par3;
+
+moving_head mov1;
+moving_head mov2; //todo control address
+
+
 void send_output(int speed);
 
 void init_fixture();
