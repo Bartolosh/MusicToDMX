385155dc4fab2fcbce55e1b2c9be4d2c197902c3
Bartolosh <matteobartolosh@gmail.com>
2021-12-30 12:45:18 +0100

    change sampling freq of lowpass filter to
    need to check our sample freq

--- musictodmx/LowPassFilter.h
+++ musictodmx/LowPassFilter.h
@@ -6,21 +6,21 @@
 FIR filter designed with
  http://t-filter.appspot.com
 
-sampling frequency: 2000 Hz
+sampling frequency: 10000 Hz
 
 * 0 Hz - 200 Hz
   gain = 1
   desired ripple = 5 dB
-  actual ripple = 3.6987047106904303 dB
+  actual ripple = 4.125292938619616 dB
 
-* 300 Hz - 1000 Hz
+* 300 Hz - 5000 Hz
   gain = 0
   desired attenuation = -40 dB
-  actual attenuation = -41.01892043065909 dB
+  actual attenuation = -40.10209527155597 dB
 
 */
 
-#define LOWPASSFILTER_TAP_NUM 25
+#define LOWPASSFILTER_TAP_NUM 117
 
 typedef struct {
   double history[LOWPASSFILTER_TAP_NUM];
